3086fbbf186ecb9f295dceed9525a98872829323
mayone-du <cocomayo1201@icloud.com>
2021-07-11 14:25:44 +0900

    Add footer links

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -4,37 +4,53 @@ import { memo } from "react";
 export const Footer: React.VFC = memo(() => {
   return (
     <footer className="px-4 md:px-64 pt-8 pb-20 lg:pb-8 bg-white dark:bg-gray-900 border-t border-gray-100">
-      <nav>
-        <h4 className="py-1 pl-2 mb-4 font-bold border-l-4 border-black dark:border-white">
-          サイト一覧
-        </h4>
-        <ul className="text-sm text-blue-600 underline dark:to-blue-400">
-          <li className="mb-2">
-            <Link href="/">
-              <a>ホーム</a>
-            </Link>
-          </li>
-          <li className="mb-2">
-            <Link href="/blog">
-              <a>ブログ</a>
-            </Link>
-          </li>
-          <li className="mb-2">
-            <Link href="/category">
-              <a>カテゴリー</a>
-            </Link>
-          </li>
-          <li className="mb-2">
-            <Link href="/tag">
-              <a>タグ</a>
-            </Link>
-          </li>
-          <li className="mb-2">
-            <Link href="/profile">
-              <a>プロフィール</a>
-            </Link>
-          </li>
-        </ul>
+      <nav className="flex w-full">
+        <div className="w-1/2">
+          <h4 className="py-1 pl-2 mb-4 font-bold border-l-4 border-black dark:border-white">
+            サイト一覧
+          </h4>
+          <ul className="text-sm text-blue-600 underline dark:to-blue-400">
+            <li className="mb-2">
+              <Link href="/">
+                <a>ホーム</a>
+              </Link>
+            </li>
+            <li className="mb-2">
+              <Link href="/blog">
+                <a>ブログ</a>
+              </Link>
+            </li>
+            <li className="mb-2">
+              <Link href="/category">
+                <a>カテゴリー</a>
+              </Link>
+            </li>
+            <li className="mb-2">
+              <Link href="/tag">
+                <a>タグ</a>
+              </Link>
+            </li>
+            <li className="mb-2">
+              <Link href="/profile">
+                <a>プロフィール</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="w-1/2">
+          <h4 className="py-1 pl-2 mb-4 font-bold border-l-4 border-black dark:border-white">
+            SNS
+          </h4>
+          <ul className="text-sm text-blue-600 underline dark:to-blue-400">
+            <li className="mb-2">
+              <a href="/">Twitter</a>
+            </li>
+            <li className="mb-2">
+              <a href="/">GitHub</a>
+            </li>
+          </ul>
+        </div>
       </nav>
       <p className="my-4 text-center">copyright&copy;&nbsp;Yuki Ishikawa</p>
     </footer>
